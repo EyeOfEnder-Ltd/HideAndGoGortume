f68bb35d826cf5e3b987e706877d9f0e4163260c
Avery246813579 <avery246813579@gmail.com>
2013-11-03 23:54:36 -0500

    Updated permissions. Added permissions for kits.

--- src/com/eyeofender/gortume/util/Permissions.java
+++ src/com/eyeofender/gortume/util/Permissions.java
@@ -19,15 +19,37 @@ public class Permissions {
 	 *
 	/****************************************************************************/
 	
-	
 	/** Group Permissions **/
 	
 	/** Signs Permissions **/
 	public Permission wand = new Permission("gortume.wand");
 	
+	/** Kit Permissions **/
+	public Permission ninja = new Permission("gortume.ninja");
+	public Permission traveler = new Permission("gortume.traveler");
+	public Permission tank = new Permission("gortume.tank");
+	public Permission spy = new Permission("gortume.tank");
+	public Permission god = new Permission("gortume.tank");
+	
 	
 	public void enablePermissions(){
 		PluginManager pm = plugin.getServer().getPluginManager();
 		pm.addPermission(wand);
+		pm.addPermission(ninja);
+		pm.addPermission(traveler);
+		pm.addPermission(tank);
+		pm.addPermission(spy);
+		pm.addPermission(god);
+	}
+	
+	public void disablePermissions(){
+		PluginManager pm = plugin.getServer().getPluginManager();
+		pm.removePermission(wand);
+		pm.removePermission(ninja);
+		pm.removePermission(traveler);
+		pm.removePermission(tank);
+		pm.removePermission(spy);
+		pm.removePermission(god);
+
 	}
 }
